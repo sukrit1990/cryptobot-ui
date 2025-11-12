424215ffc4b578f4a75e77f9021fc7ecb086c178
Sukrit <sukrit1990@gmail.com>
2025-11-12 14:48:56 +0800

    pushing deployment changes

--- vite.config.ts
+++ vite.config.ts
@@ -16,6 +16,7 @@ export default defineConfig({
         ]
       : []),
   ],
+  base: '/cryptobot-ui/', // your repo name
   resolve: {
     alias: {
       "@": path.resolve(import.meta.dirname, "client", "src"),
